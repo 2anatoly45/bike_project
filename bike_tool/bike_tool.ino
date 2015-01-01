1751cfcc2cb0e245fe362746199e799ec926be8a
Rick--D <Rick--D@users.noreply.github.com>
2014-12-31 19:44:09 -0500

    Update bike_tool.ino
    Removed wheel adjust code, changed downState to pauseState, added arc code

--- bike_tool/bike_tool.ino
+++ bike_tool/bike_tool.ino
@@ -1,23 +1,24 @@
 #define MODE 10           // Button pin numbers
-#define DOWN 9
-#define UP 8
+#define PAUSE 8
 #define MAGNET 7          // Magnet simluated with a button
 #define INTERVAL 2000     // Interval for velocity
+#define DIAMETER 27       // Diamter of the wheel in inches
+#define MAGNETS 2         // Number of magnets on the wheel (affects accuracy of measurement)
 
 boolean modeState = 0;    // 3 buttons + simulated magnet sensor
 boolean modePrev = 0;
-boolean downState = 0;
-boolean upState = 0;
+boolean pauseState = 0;
+boolean pausePrev = 0;
 boolean magnetState = 0;
 boolean magnetPrev = 0;
 
 int mag = 0;              // Times magnet passes by sensor
 int contacts = 0;         // For velocity, runs parallel to mag
-int time = 0;             // Time of trip
+int riding_time = 0;      // Time of trip in milliseconds
 int seconds = 0;
 int minutes = 0;
 int hours = 0;
-double diameter = 27;        // Wheel diameter in inches
+double arc = DIAMETER / MAGNETS;
 double distance = 0;         // Distance travelled
 double span = 0;             // Time interval between consecutive velocity calculations
 double velocity = 0;         // Instantaneous speed
@@ -30,28 +31,25 @@ int moment = 0;           // Value of millis at which up button is pressed
 void setup() {
   Serial.begin(9600);
   pinMode(MODE, INPUT);
-  pinMode(DOWN, INPUT);
-  pinMode(UP, INPUT);
+  pinMode(PAUSE, INPUT);
   pinMode(MAGNET, INPUT);
 }
 
 void loop() { 
-//  Serial.print("millis: "); Serial.println(millis()); // For verifying execution time
+  //  Serial.print("millis: "); Serial.println(millis()); // For verifying execution time
 
   modeState = digitalRead(MODE);
-  downState = digitalRead(DOWN);
-  upState = digitalRead(UP);
+  pauseState = digitalRead(PAUSE);
   magnetState = digitalRead(MAGNET);        // Will be replaced by sensor
 
-  seconds = (millis() - moment) / 1000 % 60;
-  minutes = ((millis() - moment) / 1000 % 3600) / 60;
-  hours = (millis() - moment) / 1000 / 3600;
+  convert_time(millis());       // Convert time into hours, minutes and seconds
+  convert_time(riding_time);
 
-  distance = mag * ((diameter * 0.0254) * 3.141593) / 1000;    // [km]
+  distance = mag * ((arc * 0.0254) * 3.141593) / 1000;    // [km]
   av_speed = distance * 3600 / ((millis() - moment) / 1000);   // [km/h]
 
   if (INTERVAL <= ((millis() - moment) - span)) {
-    velocity = (contacts * (diameter * 0.0254) * 3.141593) / INTERVAL * 3600;
+    velocity = (contacts * (arc * 0.0254) * 3.141593) / INTERVAL * 3600;
     contacts = 0;
     span = millis() - moment;
   }
@@ -69,7 +67,7 @@ void loop() {
     else if (counter == 2)
       counter = 0;
   }
-  
+
   if (modeState == LOW)
     modePrev = 0;
 
@@ -84,80 +82,52 @@ void loop() {
   if (magnetState == LOW)
     magnetPrev = 0;
 
-  if (counter != 2) {
-    if (upState == HIGH) {
-      moment = millis();
-    } 
-    else if (downState == HIGH) {
-      int breakStarted = millis();
+  if (pauseState == HIGH && pausePrev == 0) { // Press the pause button
 
-      while(downState != HIGH) {
-        // Do nothing (bad solution, spedometer is unusable for this period
-      }
-
-      int breakOver = millis();
-
-      int breakTime = breakOver - breakStarted;
-
-      time -= breakTime;
-    }      
-  }
-  
-/*
-  if (counter == 2) {
-    if (downState == HIGH)
-      --diameter;
-    else if (upState == HIGH)
-      ++diameter;
+    moment = millis();
   } 
-*/
+
+  // ==== Replace the following with code that will handle pressing pause button again ====
+  /* 
+   else if (downState == HIGH) {
+   int breakStarted = millis();
+   
+   while(downState != HIGH) {
+   // Do nothing (bad solution, spedometer is unusable for this period
+   }
+   
+   int breakOver = millis();
+   
+   int breakTime = breakOver - breakStarted;
+   
+   time -= breakTime;
+   }  
+   */
 
   printer();
 }
-
-/*
-double calculateSpeed() {
- int start = getTime(); // obtain initial time
- 
- int initialCount = mag; // initial number of contacts
- 
- int finalCount = 0; 
- int contacts = 0;
- int intervalSpeed = 0;
- 
- // determine how many magnet contacts have been made within the specified interval
- if(millis() >= start + INTERVAL) {
- contacts = mag - initialCount;
- intervalSpeed = contacts * ((diameter * 0.0254) * 3.141593) / 1000;
- return intervalSpeed;
- }
- 
- return 0; 
- }
- */
 
 void printer() {   //   Serial.print(mag); Serial.print("   ");       // Note: printer is limited to a 16x2 display  
   if (counter == 0) {
     Serial.print("S ");            // 2
     if (velocity < 10) Serial.print('0');    
     Serial.print(velocity, 1);     // 4
-    Serial.print("km/h");          // 4
-                                   // Total = 10
-                                Serial.print(" ");
+    Serial.print("km/h");          // 4   Total = 10
+
+    Serial.print(" ");
     Serial.print(" MAX S ");       // 6
     if (max_speed < 10) Serial.print('0');
     Serial.print(max_speed, 1);    // 4
-    Serial.println("km/h");        // 4
-                                   // Total = 14
+    Serial.println("km/h");        // 4   Total = 14
+    // 
   }
-
-  if (counter == 1) {
+  else if (counter == 1) {
     Serial.print("D ");            // 2
     if (distance < 10) Serial.print('0');    
     Serial.print(distance, 1);     // 4
-    Serial.print("km");            // 2
-                                   // Total = 8
-                            Serial.print(" ");
+    Serial.print("km");            // 2   Total = 8
+
+    Serial.print(" ");
     Serial.print("T ");            // 2
     if (hours < 10) Serial.print('0');
     Serial.print(hours);           // 2
@@ -166,23 +136,16 @@ void printer() {   //   Serial.print(mag); Serial.print("   ");       // Note: p
     Serial.print(minutes);         // 2
     Serial.print(':');             // 1
     if (seconds < 10) Serial.print('0');
-    Serial.println(seconds);       // 2
-                                   // Total = 10
+    Serial.println(seconds);       // 2   Total = 10
+
   }
-  
-  if (counter == 2) {
+  else if (counter == 2) {
     Serial.print("AVG S ");        // 6
     if (av_speed < 10) Serial.print('0');    
     Serial.print(av_speed, 1);     // 4
-    Serial.println("km/h");        // 4
-                                   // Total = 14
+    Serial.println("km/h");        // 4   Total = 14
+
     //Serial.print(time);          // Total time of trip
   }
-  
-/*
-  if (counter == 2) { // Print wheel diameter in inches
-    Serial.print("ADJ SIZE: ");
-    Serial.print(diameter);
-  }
-*/
 }
+
