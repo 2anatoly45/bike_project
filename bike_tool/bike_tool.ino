d21cd2cf503b9574ed2d6097678af5bc106f3307
Anatoly <anatoly45@hotmail.com>
2015-01-04 23:36:41 -0500

    Added a priner function for LCD

--- bike_tool/bike_tool.ino
+++ bike_tool/bike_tool.ino
@@ -1,9 +1,11 @@
-#define MODE 10           // Button pin numbers
+#include <LiquidCrystal.h>
+
+#define MODE 7            // Button pin numbers
 #define PAUSE 8
-#define MAGNET 7          // Magnet simluated with a button
-#define INTERVAL 2000     // Time interval, in milliseconds, which will be used to determine instantaneous velocity
+#define SENSOR 9          // Hall Effect Sensor (latching)
+#define INTERVAL 2000     // Time interval, in milliseconds, for determining instantaneous velocity
 #define DIAMETER 27       // Diamter of the wheel in inches
-#define MAGNETS 2         // Number of magnets on the wheel (affects accuracy of measurement)
+#define MAGNETS 2         // Number of magnets (affects accuracy of measurement)
 #define IN_TO_M 0.0254    // Conversion rate from inches to meters (1 inch = 0.0254 meters)
 #define PI 3.141593
 
@@ -20,7 +22,10 @@ int riding_time = 0;      // Time of trip in milliseconds
 int seconds = 0;
 int minutes = 0;
 int hours = 0;
-double arc = ((DIAMETER * PI) * IN_TO_M) / MAGNETS ; // Arc length between magnets in meters
+int riding_seconds = 0;
+int riding_minutes = 0;
+int riding_hours = 0;
+double arc = ((DIAMETER * PI) * IN_TO_M) / MAGNETS ; // Arc length between magnets [m]
 double distance = 0;         // Distance travelled
 double span = 0;             // Time interval between consecutive velocity calculations
 double velocity = 0;         // Instantaneous speed
@@ -30,19 +35,14 @@ double max_speed = 0;        // Max speed attained during trip
 int counter = 0;          // Used for switching between modes
 int moment = 0;           // Value of millis at which up button is pressed
 
+LiquidCrystal lcd(2, 3, 13, 12, 11, 10);
+
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
   pinMode(MODE, INPUT);
   pinMode(PAUSE, INPUT);
-  pinMode(MAGNET, INPUT);
-}
-
-void convert_time(int mil) { // -----Still need a way to adjust the session time-----
-  double sec = mil / 1000; // Convert into seconds first so that the rest of the calculations are easier to read
-  
-  seconds = sec % 60;
-  minutes = sec % 3600 / 60;
-  hours = sec / 3600;
+  pinMode(SENSOR, INPUT);
 }
 
 void loop() { 
@@ -50,18 +50,18 @@ void loop() {
 
   modeState = digitalRead(MODE);
   pauseState = digitalRead(PAUSE);
-  magnetState = digitalRead(MAGNET);        // Will be replaced by sensor
+  magnetState = digitalRead(SENSOR);
 
-  convert_time(millis());       // Convert time into hours, minutes and seconds
+  convert_time(millis());         // Convert time into hours, minutes and seconds
   convert_time(riding_time);
 
-  distance = mag * arc / 1000;    // [km]
-  av_speed = distance * 3600 / ((millis() - moment) / 1000);   // [km/h]
+  distance = mag * arc / 1000;                         // [km]
+  av_speed = distance * 3600 / (riding_time / 1000);   // [km/h]
 
-  if (INTERVAL <= ((millis() - moment) - span)) {
-    velocity = (contacts * arc) / INTERVAL * 3600;
+  if (INTERVAL <= (riding_time - span)) {
+    velocity = (contacts * arc) * 3600 / INTERVAL;
     contacts = 0;
-    span = millis() - moment;
+    span = riding_time;
   }
 
   if (max_speed < velocity) 
@@ -81,14 +81,12 @@ void loop() {
   if (modeState == LOW)
     modePrev = 0;
 
-  // Increment ONLY when the magnet has begun to be sensed
   if (magnetState == HIGH && magnetPrev == 0) {
     magnetPrev = 1;
     ++mag;
     ++contacts;
   }
 
-  // Reset previous magnet state once magnet is no longer sensed
   if (magnetState == LOW)
     magnetPrev = 0;
 
@@ -96,24 +94,14 @@ void loop() {
     moment = millis();
   } 
 
-  // ==== Replace the following with code that will handle pressing pause button again ====
-  /* 
-   else if (downState == HIGH) {
-   int breakStarted = millis();
-   
-   while(downState != HIGH) {
-   // Do nothing (bad solution, spedometer is unusable for this period
-   }
-   
-   int breakOver = millis();
-   
-   int breakTime = breakOver - breakStarted;
-   
-   time -= breakTime;
-   }  
-   */
-
   printer();
+  lcd_printer();
+}
+
+void convert_time(int mil) { // -----Still need a way to adjust the session time-----  
+  seconds = (mil / 1000) % 60;
+  minutes = (mil / 1000) % 3600 / 60;
+  hours = (mil / 1000) / 3600;
 }
 
 void printer() {   //   Serial.print(mag); Serial.print("   ");       // Note: printer is limited to a 16x2 display  
@@ -155,6 +143,61 @@ void printer() {   //   Serial.print(mag); Serial.print("   ");       // Note: p
     Serial.println("km/h");        // 4   Total = 14
 
     //Serial.print(time);          // Total time of trip
+  }
+}
+
+void lcd_printer() {               // Note: printer is limited to a 16x2 display
+  lcd.clear();
+
+  if (counter == 0) {
+    lcd.setCursor(0, 0);
+    lcd.print("S ");            // 2
+    if (velocity < 10) lcd.print('0');    
+    lcd.print(velocity, 1);     // 4
+    lcd.print("km/h");          // 4   Total = 10
+    
+    lcd.setCursor(0, 1);
+    lcd.print("MAX S ");       // 6
+    if (max_speed < 10) lcd.print('0');
+    lcd.print(max_speed, 1);    // 4
+    lcd.print("km/h");        // 4   Total = 14
+    // 
+  }
+  else if (counter == 1) {
+    lcd.setCursor(0, 0);
+    lcd.print("D ");            // 2
+    if (distance < 10) lcd.print('0');    
+    lcd.print(distance, 1);     // 4
+    lcd.print("km");            // 2   Total = 8
+
+    lcd.setCursor(0, 1);
+    lcd.print("T ");            // 2
+    if (hours < 10) lcd.print('0');
+    lcd.print(hours);           // 2
+    lcd.print(':');             // 1
+    if (minutes < 10) lcd.print('0');
+    lcd.print(minutes);         // 2
+    lcd.print(':');             // 1
+    if (seconds < 10) lcd.print('0');
+    lcd.print(seconds);         // 2   Total = 10
+    
+    lcd.setCursor(0, 1);
+    lcd.print("AVG S ");        // 6
+    if (av_speed < 10) lcd.print('0');    
+    lcd.print(av_speed, 1);     // 4
+    lcd.print("km/h");          // 4   Total = 14
+  }
+  else if (counter == 2) {
+    lcd.setCursor(0, 0);
+    lcd.print("TOT T ");         // 6
+    if (riding_hours < 10) lcd.print('0');
+    lcd.print(riding_hours);     // 2
+    lcd.print(':');              // 1
+    if (riding_minutes < 10) lcd.print('0');
+    lcd.print(riding_minutes);   // 2
+    lcd.print(':');              // 1
+    if (riding_seconds < 10) lcd.print('0');
+    lcd.print(riding_seconds);   // 2   Total = 10
   }
 }
 
